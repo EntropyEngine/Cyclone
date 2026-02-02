004391ffdd9b6277efd61c1a9d5a4f7eda1ca836
Avelina9X <luke.kyriacou.9x@gmail.com>
2026-02-02 02:29:19 +0000

    Refactored into per viewport templates

--- Cyclone/UI/ViewportElement.hpp
+++ Cyclone/UI/ViewportElement.hpp
@@ -8,6 +8,38 @@ namespace Cyclone
 {
 	namespace UI
 	{
+		enum class EViewportType
+		{
+			Perspective,
+			TopXZ,
+			FrontXY,
+			SideYZ
+		};
+
+		template<EViewportType T>
+		struct ViewportTypeTraits;
+
+		template<>
+		struct ViewportTypeTraits<EViewportType::TopXZ>
+		{
+			static constexpr size_t AxisU = 0;
+			static constexpr size_t AxisV = 2;
+		};
+
+		template<>
+		struct ViewportTypeTraits<EViewportType::FrontXY>
+		{
+			static constexpr size_t AxisU = 0;
+			static constexpr size_t AxisV = 1;
+		};
+
+		template<>
+		struct ViewportTypeTraits<EViewportType::SideYZ>
+		{
+			static constexpr size_t AxisU = 1;
+			static constexpr size_t AxisV = 2;
+		};
+
 		class ViewportElement
 		{
 		public:
--- Cyclone/UI/ViewportManager.cpp
+++ Cyclone/UI/ViewportManager.cpp
@@ -28,10 +28,10 @@ namespace
 
 Cyclone::UI::ViewportManager::ViewportManager( ID3D11Device3 *inDevice )
 {
-	mViewportPerspective = std::make_unique<Cyclone::UI::ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::CornflowerBlue );
-	mViewportTop = std::make_unique<Cyclone::UI::ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
-	mViewportFront = std::make_unique<Cyclone::UI::ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
-	mViewportSide = std::make_unique<Cyclone::UI::ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
+	mViewportPerspective = std::make_unique<ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::CornflowerBlue );
+	mViewportTop = std::make_unique<ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
+	mViewportFront = std::make_unique<ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
+	mViewportSide = std::make_unique<ViewportElement>( inDevice, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT, DirectX::Colors::Black );
 
 	mCommonStates = std::make_unique<DirectX::CommonStates>( inDevice );
 
@@ -229,211 +229,15 @@ void Cyclone::UI::ViewportManager::RenderPerspective( ID3D11DeviceContext3 *inDe
 
 void Cyclone::UI::ViewportManager::RenderTop( ID3D11DeviceContext3 *inDeviceContext )
 {
-	mViewportTop->Clear( inDeviceContext );
-
-	inDeviceContext->OMSetBlendState( mCommonStates->Opaque(), nullptr, 0xFFFFFFFF );
-	inDeviceContext->OMSetDepthStencilState( mCommonStates->DepthNone(), 0 );
-	inDeviceContext->RSSetState( mCommonStates->CullNone() );
-	inDeviceContext->IASetInputLayout( mWireFrameInputLayout.Get() );
-
-	mWireframeEffect->SetMatrices(
-		DirectX::XMMatrixIdentity(),
-		DirectX::XMMatrixLookAtRH(
-			DirectX::XMVectorSet( static_cast<float>( mCenterX2D ), 10000.0f, static_cast<float>( mCenterZ2D ), 0.0f ),
-			DirectX::XMVectorSet( static_cast<float>( mCenterX2D ), -10000.0f, static_cast<float>( mCenterZ2D ), 0.0f ),
-			DirectX::XMVectorSet( 0.0f, 0.0f, 1.0f, 0.0f )
-		),
-		DirectX::XMMatrixOrthographicRH( mViewportTop->GetWidth() * mZoomScale2D, mViewportTop->GetHeight() * mZoomScale2D, 1.0f, 20000.0f )
-	);
-	mWireframeEffect->Apply( inDeviceContext );
-
-	mWireframeBatch->Begin();
-
-	float minX = static_cast<float>( mViewportTop->GetWidth() * -mZoomScale2D / 2 + mCenterX2D );
-	float maxX = static_cast<float>( mViewportTop->GetWidth() * mZoomScale2D / 2 + mCenterX2D );
-
-	float minZ = static_cast<float>( mViewportTop->GetHeight() * -mZoomScale2D / 2 + mCenterZ2D );
-	float maxZ = static_cast<float>( mViewportTop->GetHeight() * mZoomScale2D / 2 + mCenterZ2D );
-
-	for ( float xLine = std::round( minX / mSubGridSize ) * mSubGridSize; xLine <= maxX; xLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( xLine, 0.0f, minZ, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( xLine, 0.0f, maxZ, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float zLine = std::round( minZ / mSubGridSize ) * mSubGridSize; zLine <= maxZ; zLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( minX, 0.0f, zLine, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( maxX, 0.0f, zLine, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float xLine = std::round( minX / mGridSize ) * mGridSize; xLine <= maxX; xLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( xLine, 0.0f, minZ, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( xLine, 0.0f, maxZ, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	for ( float zLine = std::round( minZ / mGridSize ) * mGridSize; zLine <= maxZ; zLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( minX, 0.0f, zLine, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( maxX, 0.0f, zLine, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( minX, 0.0f, 0.0f, 0.0f ), DirectX::Colors::DarkRed },
-		{ DirectX::XMVectorSet( maxX, 0.0f, 0.0f, 0.0f ), DirectX::Colors::DarkRed }
-	);
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( 0.0f, 0.0f, minZ, 0.0f ), DirectX::Colors::DarkBlue },
-		{ DirectX::XMVectorSet( 0.0f, 0.0f, maxZ, 0.0f ), DirectX::Colors::DarkBlue }
-	);
-
-	mWireframeBatch->End();
-
-	mViewportTop->Resolve( inDeviceContext );
+	RenderWireFrame<EViewportType::TopXZ>( inDeviceContext );
 }
 
 void Cyclone::UI::ViewportManager::RenderFront( ID3D11DeviceContext3 *inDeviceContext )
 {
-	mViewportFront->Clear( inDeviceContext );
-	
-	inDeviceContext->OMSetBlendState( mCommonStates->Opaque(), nullptr, 0xFFFFFFFF );
-	inDeviceContext->OMSetDepthStencilState( mCommonStates->DepthNone(), 0 );
-	inDeviceContext->RSSetState( mCommonStates->CullNone() );
-	inDeviceContext->IASetInputLayout( mWireFrameInputLayout.Get() );
-
-	mWireframeEffect->SetMatrices(
-		DirectX::XMMatrixIdentity(),
-		DirectX::XMMatrixLookAtRH(
-			DirectX::XMVectorSet( static_cast<float>( -mCenterX2D ), static_cast<float>( mCenterY2D ), 10000.0f, 0.0f ),
-			DirectX::XMVectorSet( static_cast<float>( -mCenterX2D ), static_cast<float>( mCenterY2D ), -10000.0f, 0.0f ),
-			DirectX::XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f )
-		),
-		DirectX::XMMatrixOrthographicRH( mViewportFront->GetWidth() * mZoomScale2D, mViewportFront->GetHeight() * mZoomScale2D, 1.0f, 20000.0f )
-	);
-	mWireframeEffect->Apply( inDeviceContext );
-
-	float minX = static_cast<float>( mViewportFront->GetWidth() * -mZoomScale2D / 2 - mCenterX2D );
-	float maxX = static_cast<float>( mViewportFront->GetWidth() * mZoomScale2D / 2 - mCenterX2D );
-
-	float minY = static_cast<float>( mViewportFront->GetHeight() * -mZoomScale2D / 2 + mCenterY2D );
-	float maxY = static_cast<float>( mViewportFront->GetHeight() * mZoomScale2D / 2 + mCenterY2D );
-
-	mWireframeBatch->Begin();
-
-	for ( float xLine = std::round( minX / mSubGridSize ) * mSubGridSize; xLine <= maxX; xLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( xLine, minY, 0.0f, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( xLine, maxY, 0.0f, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float yLine = std::round( minY / mSubGridSize ) * mSubGridSize; yLine <= maxY; yLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( minX, yLine, 0.0f, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( maxX, yLine, 0.0f, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float xLine = std::round( minX / mGridSize ) * mGridSize; xLine <= maxX; xLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( xLine, minY, 0.0f, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( xLine, maxY, 0.0f, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	for ( float yLine = std::round( minY / mGridSize ) * mGridSize; yLine <= maxY; yLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( minX, yLine, 0.0f, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( maxX, yLine, 0.0f, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( minX, 0.0f, 0.0f, 0.0f ), DirectX::Colors::DarkRed },
-		{ DirectX::XMVectorSet( maxX, 0.0f, 0.0f, 0.0f ), DirectX::Colors::DarkRed }
-	);
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( 0.0f, minY, 0.0f, 0.0f ), DirectX::Colors::Green },
-		{ DirectX::XMVectorSet( 0.0f, maxY, 0.0f, 0.0f ), DirectX::Colors::Green }
-	);
-	mWireframeBatch->End();
-
-	mViewportFront->Resolve( inDeviceContext );
+	RenderWireFrame<EViewportType::FrontXY>( inDeviceContext );
 }
 
 void Cyclone::UI::ViewportManager::RenderSide( ID3D11DeviceContext3 *inDeviceContext )
 {
-	mViewportSide->Clear( inDeviceContext );
-	
-	inDeviceContext->OMSetBlendState( mCommonStates->Opaque(), nullptr, 0xFFFFFFFF );
-	inDeviceContext->OMSetDepthStencilState( mCommonStates->DepthNone(), 0 );
-	inDeviceContext->RSSetState( mCommonStates->CullNone() );
-	inDeviceContext->IASetInputLayout( mWireFrameInputLayout.Get() );
-
-	mWireframeEffect->SetMatrices(
-		DirectX::XMMatrixIdentity(),
-		DirectX::XMMatrixLookAtRH(
-			DirectX::XMVectorSet( 10000.0f, static_cast<float>( mCenterY2D ), static_cast<float>( mCenterZ2D ), 0.0f ),
-			DirectX::XMVectorSet( -10000.0f, static_cast<float>( mCenterY2D ), static_cast<float>( mCenterZ2D ), 0.0f ),
-			DirectX::XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f )
-		),
-		DirectX::XMMatrixOrthographicRH( mViewportSide->GetWidth() * mZoomScale2D, mViewportSide->GetHeight() * mZoomScale2D, 1.0f, 20000.0f )
-	);
-	mWireframeEffect->Apply( inDeviceContext );
-
-	mWireframeBatch->Begin();
-
-	float minY = static_cast<float>( mViewportSide->GetHeight() * -mZoomScale2D / 2 + mCenterY2D );
-	float maxY = static_cast<float>( mViewportSide->GetHeight() * mZoomScale2D / 2 + mCenterY2D );
-
-	float minZ = static_cast<float>( mViewportSide->GetWidth() * -mZoomScale2D / 2 + mCenterZ2D );
-	float maxZ = static_cast<float>( mViewportSide->GetWidth() * mZoomScale2D / 2 + mCenterZ2D );
-
-	for ( float zLine = std::round( minZ / mSubGridSize ) * mSubGridSize; zLine <= maxZ; zLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( 0.0f, minY, zLine, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( 0.0f, maxY, zLine, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float yLine = std::round( minY / mSubGridSize ) * mSubGridSize; yLine <= maxY; yLine += mSubGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( 0.0f, yLine, minZ, 0.0f ), DirectX::ColorsLinear::DimGray },
-			{ DirectX::XMVectorSet( 0.0f, yLine, maxZ, 0.0f ), DirectX::ColorsLinear::DimGray }
-		);
-	}
-
-	for ( float zLine = std::round( minZ / mGridSize ) * mGridSize; zLine <= maxZ; zLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( 0.0f, minY, zLine, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( 0.0f, maxY, zLine, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	for ( float yLine = std::round( minY / mGridSize ) * mGridSize; yLine <= maxY; yLine += mGridSize ) {
-		mWireframeBatch->DrawLine(
-			{ DirectX::XMVectorSet( 0.0f, yLine, minZ, 0.0f ), DirectX::Colors::DimGray },
-			{ DirectX::XMVectorSet( 0.0f, yLine, maxZ, 0.0f ), DirectX::Colors::DimGray }
-		);
-	}
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( 0.0f, 0.0f, minZ, 0.0f ), DirectX::Colors::DarkBlue },
-		{ DirectX::XMVectorSet( 0.0f, 0.0f, maxZ, 0.0f ), DirectX::Colors::DarkBlue }
-	);
-
-	mWireframeBatch->DrawLine(
-		{ DirectX::XMVectorSet( 0.0f, minY, 0.0f, 0.0f ), DirectX::Colors::Green },
-		{ DirectX::XMVectorSet( 0.0f, maxY, 0.0f, 0.0f ), DirectX::Colors::Green }
-	);
-	mWireframeBatch->End();
-
-	mViewportSide->Resolve( inDeviceContext );
+	RenderWireFrame<EViewportType::SideYZ>( inDeviceContext );
 }
