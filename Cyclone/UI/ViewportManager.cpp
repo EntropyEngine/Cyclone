2c7fcd58d20568ca8c5a2572c63a4c16c010f28a
Avelina9X <luke.kyriacou.9x@gmail.com>
2026-02-09 19:24:53 +0000

    Refactored wireframe bounding box draw to namespace local method

--- Cyclone/UI/ViewportManager.cpp
+++ Cyclone/UI/ViewportManager.cpp
@@ -33,6 +33,50 @@ namespace
 		ImGui::SetCursorPos( { inPadding, inPadding } );
 		ImGui::Text( inText );
 	}
+
+	void DrawBoundingBox( DirectX::PrimitiveBatch<DirectX::VertexPositionColor> *inBatch, DirectX::FXMVECTOR inRebasedBoxCenter, DirectX::FXMVECTOR inBoxExtent, DirectX::FXMVECTOR inBoxColor )
+	{
+		DirectX::XMMATRIX matWorld = DirectX::XMMatrixScalingFromVector( inBoxExtent );
+		matWorld.r[3] = DirectX::XMVectorSelect( matWorld.r[3], inRebasedBoxCenter, DirectX::g_XMSelect1110 );
+
+		static const DirectX::XMVECTORF32 s_verts[8] =
+		{
+			{ { { -1.f, -1.f, -1.f, 0.f } } },
+			{ { {  1.f, -1.f, -1.f, 0.f } } },
+			{ { {  1.f, -1.f,  1.f, 0.f } } },
+			{ { { -1.f, -1.f,  1.f, 0.f } } },
+			{ { { -1.f,  1.f, -1.f, 0.f } } },
+			{ { {  1.f,  1.f, -1.f, 0.f } } },
+			{ { {  1.f,  1.f,  1.f, 0.f } } },
+			{ { { -1.f,  1.f,  1.f, 0.f } } }
+		};
+
+		static const WORD s_indices[] =
+		{
+			0, 1,
+			1, 2,
+			2, 3,
+			3, 0,
+			4, 5,
+			5, 6,
+			6, 7,
+			7, 4,
+			0, 4,
+			1, 5,
+			2, 6,
+			3, 7
+		};
+
+		DirectX::VertexPositionColor verts[8];
+
+		for (size_t i = 0; i < 8; ++i) {
+			DirectX::XMVECTOR v = DirectX::XMVector3Transform( s_verts[i], matWorld );
+			DirectX::XMStoreFloat3( &verts[i].position, v );
+			DirectX::XMStoreFloat4( &verts[i].color, inBoxColor );
+		}
+
+		inBatch->DrawIndexed( D3D_PRIMITIVE_TOPOLOGY_LINELIST, s_indices, static_cast<UINT>( std::size( s_indices ) ), verts, 8 );
+	}
 }
 
 Cyclone::UI::ViewportManager::ViewportManager()
@@ -498,46 +542,7 @@ void Cyclone::UI::ViewportManager::RenderWireframe( ID3D11DeviceContext3 *inDevi
 			Cyclone::Math::XLVector rebasedEntityPosition = ( position - mCenter2D );
 			Cyclone::Math::XLVector rebasedBoundingBoxPosition = rebasedEntityPosition + boundingBox.mCenter;
 
-			DirectX::XMMATRIX matWorld = DirectX::XMMatrixScalingFromVector( boundingBox.mExtent.ToXMVECTOR() );
-			matWorld.r[3] = DirectX::XMVectorSelect( matWorld.r[3], rebasedBoundingBoxPosition.ToXMVECTOR(), DirectX::g_XMSelect1110 );
-
-			static const DirectX::XMVECTORF32 s_verts[8] =
-			{
-				{ { { -1.f, -1.f, -1.f, 0.f } } },
-				{ { {  1.f, -1.f, -1.f, 0.f } } },
-				{ { {  1.f, -1.f,  1.f, 0.f } } },
-				{ { { -1.f, -1.f,  1.f, 0.f } } },
-				{ { { -1.f,  1.f, -1.f, 0.f } } },
-				{ { {  1.f,  1.f, -1.f, 0.f } } },
-				{ { {  1.f,  1.f,  1.f, 0.f } } },
-				{ { { -1.f,  1.f,  1.f, 0.f } } }
-			};
-
-			static const WORD s_indices[] =
-			{
-				0, 1,
-				1, 2,
-				2, 3,
-				3, 0,
-				4, 5,
-				5, 6,
-				6, 7,
-				7, 4,
-				0, 4,
-				1, 5,
-				2, 6,
-				3, 7
-			};
-
-			DirectX::VertexPositionColor verts[8];
-
-			for (size_t i = 0; i < 8; ++i) {
-				DirectX::XMVECTOR v = DirectX::XMVector3Transform( s_verts[i], matWorld );
-				DirectX::XMStoreFloat3( &verts[i].position, v );
-				DirectX::XMStoreFloat4( &verts[i].color, entityColorV );
-			}
-
-			mWireframeGridBatch->DrawIndexed( D3D_PRIMITIVE_TOPOLOGY_LINELIST, s_indices, static_cast<UINT>( std::size( s_indices ) ), verts, 8 );
+			DrawBoundingBox( mWireframeGridBatch.get(), rebasedBoundingBoxPosition.ToXMVECTOR(), boundingBox.mExtent.ToXMVECTOR(), entityColorV );
 		}
 
 
