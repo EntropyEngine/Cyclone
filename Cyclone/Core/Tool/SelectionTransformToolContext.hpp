d16055372362d7e2987ef7b1a2d3841cf89e655f
Avelina9X <luke.kyriacou.9x@gmail.com>
2026-03-18 23:25:02 +0000

    Added selection bounding box vectors to SelectionTransformToolContext

--- Cyclone/Core/Tool/SelectionTransformToolContext.hpp
+++ Cyclone/Core/Tool/SelectionTransformToolContext.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// Cyclone Components
+#include "Cyclone/Core/Component/Position.hpp"
+#include "Cyclone/Core/Component/BoundingBox.hpp"
+
 // Cyclone utils
 #include "Cyclone/Util/NonCopyable.hpp"
 
@@ -25,7 +29,32 @@ namespace Cyclone::Core::Tool
 		Cyclone::Math::Vector4D XM_CALLCONV	GetInitialPosition() const { return mInitialPosition; }
 		void Deactivate() { mActiveEntity = entt::null; }
 
+		void OnPreUpdate()
+		{
+			mSelectionMin = Cyclone::Math::Vector4D::sPosInf();
+			mSelectionMax = Cyclone::Math::Vector4D::sNegInf();
+		}
+
+		// TODO: const or const&?
+		void XM_CALLCONV IncludeSelectedEntity( const Cyclone::Core::Component::Position &inPosition, const Cyclone::Core::Component::BoundingBox &inBounds )
+		{
+			Cyclone::Math::Vector4D center = inPosition.mValue + inBounds.mValue.mCenter;
+			Cyclone::Math::Vector4D boxmax = center + inBounds.mValue.mExtent;
+			Cyclone::Math::Vector4D boxmin = center - inBounds.mValue.mExtent;
+
+			mSelectionMin = Cyclone::Math::Vector4D::sMin( mSelectionMin, boxmin );
+			mSelectionMax = Cyclone::Math::Vector4D::sMax( mSelectionMax, boxmax );
+		}
+
+		void XM_CALLCONV UpdateOnDrag( const Cyclone::Math::Vector4D inDelta )
+		{
+			mSelectionMin += inDelta;
+			mSelectionMax += inDelta;
+		}
+
 	protected:
+		Cyclone::Math::Vector4D mSelectionMin = Cyclone::Math::Vector4D::sPosInf();
+		Cyclone::Math::Vector4D mSelectionMax = Cyclone::Math::Vector4D::sNegInf();
 		Cyclone::Math::Vector4D mInitialPosition = Cyclone::Math::Vector4D::sZero();
 		entt::entity mActiveEntity = entt::null;
 	};
