b35c0123f82b0cc8fd2681bd7b05db67c5ee1311
Avelina9X <luke.kyriacou.9x@gmail.com>
2026-04-08 23:19:45 +0100

    Began path selection integration

--- Cyclone/Core/Tool/SelectionToolContext.hpp
+++ Cyclone/Core/Tool/SelectionToolContext.hpp
@@ -5,6 +5,7 @@
 
 namespace Cyclone::UI::Tool {
 	class SelectionTool;
+	class PathSelectionTool;
 }
 
 namespace Cyclone::Core {
@@ -17,6 +18,7 @@ namespace Cyclone::Core::Tool
 	{
 	public:
 		friend Cyclone::UI::Tool::SelectionTool;
+		friend Cyclone::UI::Tool::PathSelectionTool;
 		friend Cyclone::Core::EntityManager;
 
 		entt::entity			GetSelectedEntity() const			{ return mSelectedEntity; }
--- Cyclone/UI/Tool/PathSelectionTool.cpp
+++ Cyclone/UI/Tool/PathSelectionTool.cpp
@@ -1,2 +1,115 @@
 #include "pch.h"
 #include "Cyclone/UI/Tool/PathSelectionTool.hpp"
+
+// Cyclone Core
+#include "Cyclone/Core/LevelInterface.hpp"
+#include "Cyclone/Core/Component/Position.hpp"
+#include "Cyclone/Core/Component/BoundingBox.hpp"
+#include "Cyclone/Core/Component/Selectable.hpp"
+#include "Cyclone/Core/Component/Visible.hpp"
+
+// Cyclone Rendering
+#include "Cyclone/Rendering/Shader/EntityIndexShader.hpp"
+
+// Cyclone math
+#include "Cyclone/Math/Vector.hpp"
+
+// ImGui Includes
+#include <imgui_internal.h>
+
+// ImGuizmo
+#include <ImGuizmo/ImGuizmo.h>
+
+void Cyclone::UI::Tool::PathSelectionTool::OnUpdate( EViewportType inType, Cyclone::Core::LevelInterface * inLevelInterface, const ViewportData & inViewportData )
+{
+	ImGuiIO &io = ImGui::GetIO();
+
+	const bool isLeftClickShort = ( ImGui::IsMouseReleased( 0, inViewportData.mCanvasID ) || ImGui::IsMouseReleased( 0, ImGuiKeyOwner_NoOwner ) ) && io.MouseDownDurationPrev[0] < io.MouseDoubleClickTime;
+	if ( !inViewportData.mIsActive || !isLeftClickShort || ImGuizmo::IsOver() || ImGuizmo::IsUsingAny() || !mIsSelected ) return;
+
+	auto &entityManager = inLevelInterface->GetEntityManager();
+	if ( !entityManager.CanAquireActionLock() ) return;
+
+	bool ctrlHeld = ImGui::IsKeyDown( ImGuiMod_Ctrl );
+	bool shiftHeld = ImGui::IsKeyDown( ImGuiMod_Shift );
+
+	if ( !( ctrlHeld || shiftHeld ) ) {}
+
+	auto &selectionContext = inLevelInterface->GetSelectionCtx();
+
+	// Not a reference; need original
+	const std::set<entt::entity> previousSelectedEntities = selectionContext.GetSelectedEntities();
+	const entt::entity previousSelectedEntity = selectionContext.GetSelectedEntity();
+	std::set<entt::entity> selectionCandidates;
+
+	entt::registry &registry = inLevelInterface->GetRegistry();
+	const entt::registry &cregistry = registry;
+
+	entt::entity hovered = inViewportData.mEntityIndexShader->ReadClosestEntity( inViewportData.mDeviceContext, inViewportData.mEntitySRV, static_cast<size_t>( inViewportData.mAbsoluteMouse.x ), static_cast<size_t>( inViewportData.mAbsoluteMouse.y ) );
+	if ( hovered != entt::null ) {
+		selectionCandidates.insert( static_cast<entt::entity>( entt::to_entity( hovered ) ) );
+	}
+
+	std::erase_if( selectionCandidates, [&cregistry, &entityManager]( entt::entity inEntity ) {
+		if ( !static_cast<bool>( cregistry.get<Cyclone::Core::Component::Selectable>( inEntity ) ) ) return true;
+		if ( !static_cast<bool>( cregistry.get<Cyclone::Core::Component::Visible>( inEntity ) ) ) return true;
+
+		const auto entityType = cregistry.get<Cyclone::Core::Component::EntityType>( inEntity );
+		const auto entityCategory = cregistry.get<Cyclone::Core::Component::EntityCategory>( inEntity );
+
+		if ( static_cast<entt::id_type>( entityCategory ) != "path"_hs.value() ) return true;
+
+		if ( !entityManager.GetEntityTypeIsVisible( entityType ) ) return true;
+		if ( !entityManager.GetEntityTypeIsSelectable( entityType ) ) return true;
+
+		if ( !entityManager.GetEntityCategoryIsVisible( entityCategory ) ) return true;
+		if ( !entityManager.GetEntityCategoryIsSelectable( entityCategory ) ) return true;
+
+		return false;
+	} );
+
+	if ( shiftHeld ) {
+		for ( auto e : selectionCandidates ) {
+			selectionContext.AddSelectedEntity( e );
+		}
+		if ( previousSelectedEntity != entt::null ) {
+			selectionContext.AddSelectedEntity( previousSelectedEntity );
+		}
+	}
+	else if ( ctrlHeld ) {
+	}
+	else {
+		selectionContext.ClearSelection();
+	}
+
+	if ( !selectionCandidates.empty() ) {
+		if ( selectionCandidates == selectionContext.mPreviousCandidates ) {
+			auto it = selectionCandidates.upper_bound( previousSelectedEntity );
+			if ( it == selectionCandidates.end() ) {
+				it = selectionCandidates.begin();
+			}
+
+			if ( ctrlHeld && previousSelectedEntities.contains( *it ) ) selectionContext.DeselectEntity( *it );
+			else selectionContext.AddSelectedEntity( *it );
+		}
+		else {
+			if ( ctrlHeld && previousSelectedEntities.contains( *selectionCandidates.begin() ) ) selectionContext.DeselectEntity( *selectionCandidates.begin() );
+			else selectionContext.AddSelectedEntity( *selectionCandidates.begin() );
+		}
+	}
+
+	selectionContext.mPreviousCandidates = selectionCandidates;
+
+	if ( selectionContext.mDirty && entityManager.IsSelectionModified() ) {
+		entityManager.BeginAction();
+		entityManager.EndAction( registry );
+
+		auto &transfromContext = inLevelInterface->GetSelectionTransformCtx();
+		transfromContext.OnPreUpdate();
+		for ( const entt::entity entity : selectionContext.GetSelectedEntities() ) {
+			const auto &[position, box] = registry.get<Cyclone::Core::Component::Position, Cyclone::Core::Component::BoundingBox>( entity );
+
+			transfromContext.IncludeSelectedEntity( position, box );
+		}
+	}
+}
